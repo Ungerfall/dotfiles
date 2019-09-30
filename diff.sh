63332918c7b8de0911d45cab34c36db0a41ba0d8
Leonid Petrov1 <leonid_petrov1@epam.com>
2019-09-30 20:31:20 +0400

    Add shell script that checks diff for all dotfiles in repo

---
+++ diff.sh
@@ -0,0 +1,21 @@
+# files
+# _vimrc
+# _vsvimrc
+# mswin.vim
+# azuredatastudio/keybindings.json
+# azuredatastudio/settings.json
+# azuredatastudio/keybindings.json
+# azuredatastudio/snippets/sql.json
+# vimfiles/autoload/plug.vim
+# vimfiles/colors/kuroi.vim
+# vimfiles/colors/solarized.vim
+
+diff -q _vimrc ~/_vimrc \
+  & diff -q _vsvimrc ~/_vsvimrc \
+  & diff -q mswin.vim "C:/Program Files (x86)/Vim/vim81/mswin.vim" \
+  & diff -q azuredatastudio/keybindings.json "C:\Users\Leonid_Petrov1\AppData\Roaming\azuredatastudio\User\keybindings.json" \
+  & diff -q azuredatastudio/settings.json "C:\Users\Leonid_Petrov1\AppData\Roaming\azuredatastudio\User\settings.json" \
+  & diff -q azuredatastudio/snippets/sql.json "C:\Users\Leonid_Petrov1\AppData\Roaming\azuredatastudio\User\snippets\sql.json" \
+  & diff -q vimfiles/autoload/plug.vim "C:\Users\Leonid_Petrov1\vimfiles\autoload\plug.vim" \
+  & diff -q vimfiles/colors/kuroi.vim "C:\Users\Leonid_Petrov1\vimfiles\colors\kuroi.vim" \
+  & diff -q vimfiles/colors/solarized.vim "C:\Users\Leonid_Petrov1\vimfiles\colors\solarized.vim" \
