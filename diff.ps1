a2c45c4aacd2c87697c8ecabec8c63e1ee087b77
Leonid Petrov1 <leonid_petrov1@epam.com>
2023-10-29 14:00:45 +0300

    Use hashtable

--- diff.ps1
+++ diff.ps1
@@ -10,12 +10,18 @@
 # vimfiles/colors/kuroi.vim
 # vimfiles/colors/solarized.vim
 
-[bool](diff (cat _vimrc) (cat ~/_vimrc))
-[bool](diff (cat _vsvimrc) (cat ~/_vsvimrc))
-[bool](diff (cat mswin.vim) (cat "C:/Program Files/Vim/vim90/mswin.vim"))
-[bool](diff (cat azuredatastudio/keybindings.json) (cat "~\AppData\Roaming\azuredatastudio\User\keybindings.json"))
-[bool](diff (cat azuredatastudio/settings.json) (cat "~\AppData\Roaming\azuredatastudio\User\settings.json"))
-[bool](diff (cat azuredatastudio/snippets/sql.json) (cat "~\AppData\Roaming\azuredatastudio\User\snippets\sql.json"))
-[bool](diff (cat vimfiles/autoload/plug.vim) (cat "~\vimfiles\autoload\plug.vim"))
-[bool](diff (cat vimfiles/colors/kuroi.vim) (cat "~\vimfiles\colors\kuroi.vim"))
-[bool](diff (cat vimfiles/colors/solarized.vim) (cat "~\vimfiles\colors\solarized.vim"))
+$files = @{
+  "_vimrc" = "~/_vimrc";
+  "_vsvimrc" = "~/_vsvimrc";
+  "mswin.vim" = "C:/Program Files/Vim/vim90/mswin.vim";
+  "azuredatastudio/keybindings.json" = "~\AppData\Roaming\azuredatastudio\User\keybindings.json";
+  "azuredatastudio/settings.json" = "~\AppData\Roaming\azuredatastudio\User\settings.json";
+  "azuredatastudio/snippets/sql.json" = "~\AppData\Roaming\azuredatastudio\User\snippets\sql.json";
+  "vimfiles/autoload/plug.vim" = "~\vimfiles\autoload\plug.vim";
+  "vimfiles/colors/kuroi.vim" = "~\vimfiles\colors\kuroi.vim";
+  "vimfiles/colors/solarized.vim" = "~\vimfiles\colors\solarized.vim";
+}
+
+$files.GetEnumerator() | % {
+  "`"$($_.Key)`" `"$($_.Value)`" are different: $([bool](diff (cat $_.Key) (cat $_.Value)))"
+}
